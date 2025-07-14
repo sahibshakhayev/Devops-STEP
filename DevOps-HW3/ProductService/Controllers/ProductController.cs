8c92bbfff8fc7192b790dae5b77ecee40ff575a1
Sahib Shakhayev <sahib.shakhayev@hotmail.com>
2025-07-14 18:50:10 +0400

    final task

---
+++ DevOps-HW3/ProductService/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("list")]
+        public IActionResult GetProducts()
+        {
+            return Ok(new[] { "Apple", "Banana", "Orange" });
+        }
+    }
+}
---
+++ DevOps-HW3/UserService/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("info")]
+        public IActionResult GetUserInfo()
+        {
+            return Ok(new { Name = "Sahib Shakhayev", Role = "Admin" });
+        }
+    }
+
+}
